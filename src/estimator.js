66832e5d3049dcfdc250a63046b0200a573ad46f
jvicmaina <52795680+jvicmaina@users.noreply.github.com>
2020-04-13 22:15:54 +0300

    the estimation function
    yet to debug

--- src/estimator.js
+++ src/estimator.js
@@ -1,4 +1,38 @@
 //first addition of code
 const covid19ImpactEstimator = (data) => data;
-
 export default covid19ImpactEstimator;
+{  
+     data: {
+        {   region: {    
+            name: "Africa"  ; 
+             avgAge: 19.7    ; 
+              avgDailyIncomeInUSD: 5 ; 
+         avgDailyIncomePopulation: 0.71;   
+       }
+          periodType: "days","weeks","months";
+           timeToElapse: 58  ;
+           reportedCases: 674 ; 
+           population: 66622705;
+           totalHospitalBeds: 1380614;
+     }          // the input data you got  
+      impact: {
+        
+          impact.currentlyInfected + "is" ; reportedCases * 10 ;
+        impact.infectionsByRequestedTime + "is"; currentlyInfected *1024;
+        impact.severeCasesByRequestedTime +"is";15 % infectionsByRequestedTime;
+        impact.hospitalBedsByRequestedTime + "is" ;35% totalHospitalBeds ;
+        impact.casesForICUByRequestedTime + "is"; 5% infectionsByRequestedTime;
+        impact.dollarsInFlight +  "is";avgDailyIncomeInUSD*30*(0.65*infectionsByRequestedTime);
+
+      }        // your best case estimation   
+     severeImpact: {
+        severeImpact.currentlyInfected + "is" ; reportedCases* 50;
+        severeImpact.infectionsByRequestedTime +"is"; currentlyInfected *1024;
+        severeImpact.severeCasesByRequestedTime +"is";15 % infectionsByRequestedTime;
+        severeImpact.hospitalBedsByRequestedTime + "is" ;5 % totalHospitalBeds ;
+        severeImpact.hospitalBedsByRequestedTime + "is" ;10 % totalHospitalBeds ;
+        severeImpact.casesForVentilatorsByRequestedTime  +"is"; 2 % infectionsByRequestedTime;
+     }   // your severe case estimation
+    }
+}
+
