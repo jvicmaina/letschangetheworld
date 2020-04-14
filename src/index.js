272b1d576fd5acd5548755f8672cfd9e3197e2cb
jvicmaina <52795680+jvicmaina@users.noreply.github.com>
2020-04-14 10:20:41 +0300

    index.js created

---
+++ src/index.js
@@ -0,0 +1,19 @@
+function computationData()=>{
+
+}
+
+
+export default 
+ const data: {
+        {   region: {    
+            name: "Africa"  ; 
+             avgAge: 19.7    ; 
+              avgDailyIncomeInUSD: 5 ; 
+         avgDailyIncomePopulation: 0.71;   
+       }
+         periodType: "days","weeks","months";
+           timeToElapse: 58  ;
+           reportedCases: 674 ; 
+           population: 66622705;
+           totalHospitalBeds: 1380614;
+     }          
