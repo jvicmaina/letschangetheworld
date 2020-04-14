de98111084c9b9e0abe0b2c94258f57e973c7d09
jvicmaina <52795680+jvicmaina@users.noreply.github.com>
2020-04-14 14:37:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
-function computationData(periodType,impact,severeImpact)=>{
-
-}
 
 import covid19ImpactEstimator;
-export default 
- const data: {
-        {   region: {    
-            name: "Africa"  ; 
-             avgAge: 19.7    ; 
-              avgDailyIncomeInUSD: 5 ; 
-         avgDailyIncomePopulation: 0.71;   
-       }
-         periodType: "days","weeks","months";
-           timeToElapse: 58  ;
-           reportedCases: 674 ; 
-           population: 66622705;
-           totalHospitalBeds: 1380614;
-     } 
+function computationData(impact,severeImpact)=>{
+ Export default;
   impact: {
         
-          impact.currentlyInfected + "is" ; reportedCases * 10 ;
+        impact.currentlyInfected + "is" ; reportedCases * 10 ;
         impact.infectionsByRequestedTime + "is"; currentlyInfected *1024;
         impact.severeCasesByRequestedTime +"is";15 % infectionsByRequestedTime;
         impact.hospitalBedsByRequestedTime + "is" ;35% totalHospitalBeds ;
@@ -35,3 +20,6 @@ export default
         severeImpact.hospitalBedsByRequestedTime + "is" ;10 % totalHospitalBeds ;
         severeImpact.casesForVentilatorsByRequestedTime  +"is"; 2 % infectionsByRequestedTime;
      }   
+
+
+}
