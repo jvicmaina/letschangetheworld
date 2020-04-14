78b714f87c9870ef0f3151886fc82d521af2abb3
jvicmaina <52795680+jvicmaina@users.noreply.github.com>
2020-04-14 10:24:47 +0300

    update index js

--- src/index.js
+++ src/index.js
@@ -16,4 +16,22 @@ export default
            reportedCases: 674 ; 
            population: 66622705;
            totalHospitalBeds: 1380614;
-     }          
+     } 
+  impact: {
+        
+          impact.currentlyInfected + "is" ; reportedCases * 10 ;
+        impact.infectionsByRequestedTime + "is"; currentlyInfected *1024;
+        impact.severeCasesByRequestedTime +"is";15 % infectionsByRequestedTime;
+        impact.hospitalBedsByRequestedTime + "is" ;35% totalHospitalBeds ;
+        impact.casesForICUByRequestedTime + "is"; 5% infectionsByRequestedTime;
+        impact.dollarsInFlight +  "is";avgDailyIncomeInUSD*30*(0.65*infectionsByRequestedTime);
+
+      }      
+     severeImpact: {
+        severeImpact.currentlyInfected + "is" ; reportedCases* 50;
+        severeImpact.infectionsByRequestedTime +"is"; currentlyInfected *1024;
+        severeImpact.severeCasesByRequestedTime +"is";15 % infectionsByRequestedTime;
+        severeImpact.hospitalBedsByRequestedTime + "is" ;5 % totalHospitalBeds ;
+        severeImpact.hospitalBedsByRequestedTime + "is" ;10 % totalHospitalBeds ;
+        severeImpact.casesForVentilatorsByRequestedTime  +"is"; 2 % infectionsByRequestedTime;
+     }   
