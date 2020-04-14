1fad0bfa978111a52794d3fa2e673c4afb18fb7b
jvicmaina <52795680+jvicmaina@users.noreply.github.com>
2020-04-14 23:52:32 +0300

    trial2

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,74 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+currentlyInfected =infected(data.reportedCases)
+infectionsByRequestedTime =infectionsAtTimeX (currentlyInfected, data.timeToElapse)
+severeCasesByRequestedTime =severeAtTimeX (totalHospitalBeds,infectionsByRequestedTime)
+hospitalBedsByRequestedTime=bedsAtTimeX(totalHospitalBeds,infectionsByRequestedTime)
+casesForICUByRequestedTime=casesForIcuAtTimeX(infectionsByRequestedTime)
+casesForVentilatorsByRequestedTime=casesForVentilatorAtTimeX(infectionsByRequestedTime)
+dollarsInFlight= dollarsInFlightForEconomyX(avgDailyIncomeInUSD,data.timeToElapse,avgDailyIncomePopulation)
+};
+
+       const infected = (reportedCases) =>{
+          return{
+             impact: reportedCases*10,
+             severeImpact:reportedCases *50
+          }
+       };
+       //infections here being infections by requested time
+     const infectionsAtTimeX=(currentlyInfected,timeToLapse)=>{
+
+        infectionsByRequestedTime=currentlyInfected.impact *(Math.pow(2,Math.trunc(timeToElapse/3)))
+        severeInfectionsByRequestedTime=currentlyInfected.severeImpact *(Math.pow(2,Math.trunc(timeToElapse/3)))
+        
+        return {
+           impact:infectionsByRequestedTime,
+           severeImpact:infectionsByRequestedTime,
+           
+         }
+      
+         const severeAtTimeX=(infectionsByRequestedTime)=>{
+        severeCasesByRequestedTime= infectionsByRequestedTime.impact *(0.15)
+        severeCasesByRequestedTime= infectionsByRequestedTime.severeImpact *(0.15)
+         return{
+            impact:severeCasesByRequestedTime*0.15,
+            severeImpact:severeCasesByRequestedTime*0.15
+         }
+      }
+      //const severeAtTimeX
+     // severeCasesByRequestedTime=(0.15*infectionsByRequestedTime)
+      const bedsAtTimeX=(severeCasesByRequestedTime,totalHospitalBeds)=>{
+         hospitalBedsByRequestedTime= severeCasesByRequestedTime.impact - (totalHospitalBeds *(0.35))
+         hospitalBedsByRequestedTime= severeCasesByRequestedTime.severeImpact - (totalHospitalBeds *(0.35))
+         return{
+            impact:hospitalBedsByRequestedTime,
+            severeImpact:hospitalBedsByRequestedTime
+         }
+
+      }
+    const casesForIcuAtTimeX=(infectionsByRequestedTime)=>{
+casesForICUByRequestedTime=severeCasesByRequestedTime.impact*(0.05)
+casesForICUByRequestedTime=severeCasesByRequestedTime.severeImpact*(0.05)
+return{
+   impact:casesForICUByRequestedTime,
+   severeImpact:casesForICUByRequestedTime
+}
+      }
+    const casesForVentilatorsAtTimeX=()=>{
+      casesForVentilatorsByRequestedTime=severeCasesByRequestedTime.impact*(0.02)
+      casesForVentilatorsByRequestedTime=severeCasesByRequestedTime.severeImpact*(0.02)
+      return{
+         impact:casesForVentilatorsByRequestedTime,
+         severeImpact:casesForVentilatorsByRequestedTime
+      }
+
+    }
+    const dollarsInFlightForEconomyX =(avgDailyIncomeInUSD,avgDailyIncomePopulation)=>{
+       dollarsInFlight=infectionsByRequestedTime.impact *avgDailyIncomePopulation*(avgDailyIncomeInUSD*timeToElapse)
+       dollarsInFlight=infectionsByRequestedTime.severeImpact *avgDailyIncomePopulation*(avgDailyIncomeInUSD*timeToElapse)
+       return{
+          impact:dollarsInFlight,
+          severeImpact:dollarsInFlight
+       }
+    }
 
 export default covid19ImpactEstimator;
