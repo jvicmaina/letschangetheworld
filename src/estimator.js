2345f90b1f06c25d975ceb3d64ad010277a524bf
jvicmaina <52795680+jvicmaina@users.noreply.github.com>
2020-04-14 14:41:39 +0300

    Update estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,16 @@
-const covid19ImpactEstimator = (data) => data;
 import index.js
+const covid19ImpactEstimator = (data) => data;
+data: {
+        {   region: {    
+            name: "Africa"  ; 
+             avgAge: 19.7    ; 
+              avgDailyIncomeInUSD: 5 ; 
+         avgDailyIncomePopulation: 0.71;   
+       }
+          periodType: "days","weeks","months";
+           timeToElapse: 58  ;
+           reportedCases: 674 ; 
+           population: 66622705;
+           totalHospitalBeds: 1380614;
+     }          // the input data you got
 export default covid19ImpactEstimator;
